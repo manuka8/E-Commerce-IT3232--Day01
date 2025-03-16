ec98048a066aa60c1567c9126a773412a2655a1f
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-03-16 23:19:05 +0530

    Calculate total marks

--- question6.java
+++ question6.java
@@ -10,4 +10,21 @@ class Student {
     }
 }
 
+public class question6 {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
 
+        students.add(new Student("Amal", 85));
+        students.add(new Student("Gihan", 45));
+        students.add(new Student("Saman", 92));
+        students.add(new Student("Lakmal", 70));
+        students.add(new Student("Gayani", 88));
+
+
+        int totalMarks = 0;
+        for (Student s : students) {
+            totalMarks += s.marks;
+        }
+        
+    }
+}
