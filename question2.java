6039c80249ae9d3902c6af2fe5c1037936d726db
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-03-16 21:46:46 +0530

    Create main class and get user input

--- question2.java
+++ question2.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class question2 {
-    
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        scanner.close();
+    }
 }
