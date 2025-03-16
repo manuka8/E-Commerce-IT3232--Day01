1be2340c57411beeb2bf5e7a019eba87bffe5ddb
Manuka Mayurajith <manukamayurajith2001@gmail.com>
2025-03-16 21:55:05 +0530

    create loop and print output

--- question2.java
+++ question2.java
@@ -5,5 +5,14 @@ public class question2 {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
+
+        for (int i = 1; i <= n; i++) {
+            int m = n / 2;
+            if (i <= m) {
+                System.out.print((m - i + 1) + " ");  
+            } else {
+                System.out.print((n - (i - m) + 1) + " "); 
+            }
+        }
     }
 }
